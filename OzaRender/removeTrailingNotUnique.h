93864e0e1f2ec9197869a738b435a703021442db
Vishal Oza <Vishal.Oza@amsusa1.onmicrosoft.com>
2018-06-27 12:37:21 -0500

    first setup

---
+++ OzaRender/removeTrailingNotUnique.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <algorithm>
+#include <xmemory>
+
+#pragma pack(push,_CRT_PACKING)
+#pragma warning(push,3)
+#pragma push_macro("new")
+#undef new
+#pragma warning(disable: 4244)
+
+_STD_BEGIN
+// TEMPLATE FUNCTION remove_trailing_not_unique
+template<class _FwdIt, class _Pr> inline
+_FwdIt _Remove_trailing_not_unique(_FwdIt _First, _FwdIt _Last, _Pr _Pred)
+{	// remove each matching _Val
+	if (_First != _Last)
+	{
+		_FwdIt _Firstb = _First;
+		_FwdIt _Firstc = _First;
+		for (; ++_First != _Last; )
+			if (!_Pred(*_Firstb, *_First))
+				*++_Firstc = _STD move(*_First);
+		return (_Rechecked(++_Firstb, _Remove_trailing_not_unique(_Unchecked(_Firstb), _Unchecked(++_Firstc), _Pred)));
+	}
+	return (_Last);
+}
+
+template<class _FwdIt, class _Pr> inline
+_FwdIt remove_trailing_not_unique(_FwdIt _First, _FwdIt _Last, _Pr _Pred)
+{	// remove 
+	_DEBUG_RANGE(_First, _Last);
+	//_DEBUG_POINTER_IF(_First != _Last && _STD next(_First) != _Last, _Pred);
+	return (_Rechecked(_First,
+		_Remove_trailing_not_unique(_Unchecked(_First), _Unchecked(_Last), _Pred)));
+}
+
+template<class _FwdIt> inline
+_FwdIt remove_trailing_not_unique(_FwdIt _First, _FwdIt _Last)
+{
+	return (_STD remove_trailing_not_unique(_First, _Last, equal_to<>()));
+}
+
+_STD_END
+#pragma pop_macro("new")
+#pragma warning(pop)
+#pragma pack(pop)
---
+++ OzaRenderTest/unittest1.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include "CppUnitTest.h"
+#include "../OzaRender/removeTrailingNotUnique.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace OzaRenderTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+
+		TEST_METHOD(TestRemoveTrailingNotUniqueVectorIntNonRepeating1)
+		{
+			// TODO: Your test code here
+			std::vector<int> testVector = { 1,5,7,3,9,12 };
+			std::vector<int> checkVector = { 1,5,7,3,9,12 };
+			Assert::IsTrue(testVector.end() == std::remove_trailing_not_unique(testVector.begin(), testVector.end()), L"Unexcepted return value");
+			Assert::IsTrue(std::equal(testVector.begin(), testVector.end(), checkVector.begin(), checkVector.end()), L"tests did not match testVector with result vector");
+		}
+
+		TEST_METHOD(TestRemoveTrailingNotUniqueVectorIntRepeating1)
+		{
+			// TODO: Your test code here
+			std::vector<int> testVector = { 1,5,1,7,5,1,7,3,9,3,12 };
+			std::vector<int> checkVector = { 1,5,7,3,9,12 };
+			testVector.erase(std::remove_trailing_not_unique(testVector.begin(), testVector.end()), testVector.end());
+			Assert::IsTrue(std::equal(testVector.begin(), testVector.end(), checkVector.begin(), checkVector.end()), L"tests did not match testVector with result vector");
+		}
+	};
+}
